8b33bb238327dcf3601ed4d5e41f50972824399c
shaffer-wv <shaffer-wv@outlook.com>
2013-06-29 15:04:41 -0400

    Add status validations and tests

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,4 +1,10 @@
 class Status < ActiveRecord::Base
   attr_accessible :content, :user_id
   belongs_to :user
+
+  validates :content, presence: true
+  validates_length_of :content, :minimum => 2
+  # these can be combined as validates :content, presence: true, length: { minimum: 2 }
+
+  validates :user_id, presence: true
 end
