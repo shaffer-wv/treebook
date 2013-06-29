f3d3310c1c8379105637935e46d79e2abfde33a0
shaffer-wv <shaffer-wv@outlook.com>
2013-06-29 09:30:58 -0400

    Added tests for user informtion correctness.

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -4,4 +4,43 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "a user should enter a first name" do
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:first_name].empty?
+  end
+
+  test "a user should enter a last name" do
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:last_name].empty?
+  end
+
+  test "a user should enter a profile name" do
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:profile_name].empty?
+  end
+
+  test "user should have a unique profile name" do
+    user = User.new
+  	user.profile_name = "jshaffer"
+  	user.first_name = "Jason"
+  	user.last_name = "Shaffer"
+  	user.email = "jshaffer@nowhere.com"
+  	user.password = "password"
+  	user.password_confirmation = "password"
+  	assert !user.save
+  	assert !user.errors[:profile_name].empty?
+  end
+
+  test "a user should have a profile name without spaces" do
+    user = User.new
+    user.profile_name = "My name with spaces"
+
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+    assert user.errors[:profile_name].include?("Must be formatted correctly.")
+  end
 end
